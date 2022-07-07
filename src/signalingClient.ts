9dd990d0c2a033178e6f1eb15160166cfe9189d2
Sönke Hahn <soenkehahn@gmail.com>
2022-07-07 16:49:02 -0400

    Use types for parsing of confirmation messages

--- src/signalingClient.ts
+++ src/signalingClient.ts
@@ -1,3 +1,5 @@
+import { parseJSON } from "./apiClient/types";
+import * as t from "./apiClient/types";
 import { Channel, websocketChannel } from "./utils/channel";
 
 export type HasColor = { color: "blue" | "green" };
@@ -39,37 +41,11 @@ export const connect = async ({
 
 type Confirmation = { success: boolean } & HasColor;
 
-const parseConfirmation = (json: string): Confirmation => {
-  const error = (message: string) =>
-    new Error(
-      `parse error for confirmation message: ${message}\nreceived: ${json}`
-    );
-  let parsed: unknown = null;
-  try {
-    parsed = JSON.parse(json);
-  } catch (e) {
-    if (e instanceof SyntaxError) {
-      throw error("invalid json");
-    } else {
-      throw e;
-    }
-  }
-  if (typeof parsed !== "object" || parsed === null) {
-    throw error("expected: object");
-  }
-  if (!hasSuccess(parsed)) {
-    throw error("field not found or invalid: success");
-  }
-  if (!hasColor(parsed)) {
-    throw error("field not found or invalid: color");
-  }
-  return parsed;
+const Confirmation = {
+  success: t.boolean,
+  color: t.union(t.literal("blue"), t.literal("green")),
 };
 
-const hasSuccess = (parsed: {}): parsed is { success: boolean } =>
-  (parsed as Confirmation).success !== undefined &&
-  typeof (parsed as Confirmation).success === "boolean";
-
-const hasColor = (parsed: {}): parsed is HasColor =>
-  (parsed as Confirmation).color === "blue" ||
-  (parsed as Confirmation).color === "green";
+const parseConfirmation = (json: string): Confirmation => {
+  return parseJSON(Confirmation, json);
+};
